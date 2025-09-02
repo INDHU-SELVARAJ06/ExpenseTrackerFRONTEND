704de528356ea5741028f29bda7ca9326d055f8e
INDHU-SELVARAJ06 <indhuselvaraj06@gmail.com>
2025-09-02 20:22:57 +0530

    Fix ExpenseItem import path

--- src/Component/History.jsx
+++ src/Component/History.jsx
@@ -1,8 +1,31 @@
+// import React from 'react'
+// // import ExpenseItem from './ExpenseItem
+// import ExpenseItem from './Expenseitem'
+
+// // import ExpenseItem from './ExpenseItem'
+
+
+// function History(props) {
+//   // console.log(props.expense)
+//   return (
+//     <div className='history'>
+
+//         <h1>History</h1>
+//    {
+//      props.expense.map((item)=>(
+//         <ExpenseItem key={item.id} expense={item} deleteExpense={props.deleteExpense}/>
+
+//          ))
+//    }
+//     </div>
+//   )
+// }
+
+// export default History
+
+
 import React from 'react'
-// import ExpenseItem from './ExpenseItem
-
 import ExpenseItem from './ExpenseItem'
-
 
 function History(props) {
   // console.log(props.expense)
